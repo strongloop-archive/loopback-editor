13de3ae172c1ff164e0c1dfcab543ecfb7340845
Dallon Feldner <dallonfdev@gmail.com>
2013-04-30 13:20:25 -0700

    Started work on wizard rendering

--- editor-core/generators/resource-from-datasource/data-wizard.js
+++ editor-core/generators/resource-from-datasource/data-wizard.js
@@ -1,7 +1,9 @@
 var wizard = require('.');
 
 wizard.app.get('/', function(req, res) {
-  res.end("Hello, wizard!");
+  console.log('rendering');
+  console.log('wizard project', wizard.app.get('project'));
+  res.render('data-wizard.ejs');
 });
 
 wizard.app.get('/data', function(req, res) {
