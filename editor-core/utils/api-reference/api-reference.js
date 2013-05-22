60bebb0c314c19f73eafb00c20d9c64687a45294
Dallon Feldner <dallonfdev@gmail.com>
2013-05-22 10:50:00 -0700

    Added subpages to editor utilities

--- editor-core/utils/api-reference/api-reference.js
+++ editor-core/utils/api-reference/api-reference.js
@@ -1,5 +1,13 @@
 var utility = require('.');
 
-utility.app.get('/', function(req, res) {
-  res.render('index.ejs');
+utility.app.get('/rest', function(req, res) {
+  res.render('index.ejs', {api: "REST API"});
+});
+
+utility.app.get('/ios', function(req, res) {
+  res.render('index.ejs', {api: "iOS SDK"});
+});
+
+utility.app.get('/android', function(req, res) {
+  res.render('index.ejs', {api: "Android SDK"});
 });
