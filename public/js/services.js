2ddea06143e73dfd3aad98ab23990c7848e665b4
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-07-12 11:19:49 -0700

    Initial implementation of Open Project and Create Project.

--- public/js/services.js
+++ public/js/services.js
@@ -3,24 +3,28 @@
     .service('Workspace', function ($http, $q) {
       var Workspace = {};
 
+      function pickData(response) {
+        return response.data;
+      }
+
       Workspace.getProjects = getProjects;
       function getProjects() {
-        return $http.get('/projects');
+        return $http.get('/projects').then(pickData);
       }
 
       Workspace.getProject = getProject;
       function getProject(name) {
-        return $http.get('/projects/' + name);
+        return $http.get('/projects/' + name).then(pickData);
       }
 
       Workspace.createProject = createProject;
       function createProject(name, options) {
-        return $http.put('/projects/' + name, options || {});
+        return $http.put('/projects/' + name, options || {}).then(pickData);
       }
 
       Workspace.removeProject = removeProject;
       function removeProject(name) {
-        return $http['delete']('/projects/' + name);
+        return $http['delete']('/projects/' + name).then(pickData);
       }
 
       Workspace.addModuleToProject = addModuleToProject;
@@ -29,12 +33,12 @@
           return $q.reject(new TypeError('Expected options to include a type, but none specified.'));
         }
 
-        return $http.put('/projects/' + name + '/modules/' + subname, options);
+        return $http.put('/projects/' + name + '/modules/' + subname, options).then(pickData);
       }
 
       Workspace.removeModuleFromProject = removeModuleFromProject;
       function removeModuleFromProject(name, subname) {
-        return $http['delete']('/projects/' + name + '/modules/' + subname);
+        return $http['delete']('/projects/' + name + '/modules/' + subname).then(pickData);
       }
 
       return Workspace;
