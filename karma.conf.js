fa2d54c316b686395a7875b348a19a07be364d57
Ryan Graham <ryan@strongloop.com>
2013-12-10 17:34:02 -0800

    Update karma, make it better match Jenkins
    Also removed unused devDependencies: mocha
    
    Karma now generates xunit.xml and cobertura-coverage.xml reports to match
    what build-framework would normally do if mocha was detected.

--- karma.conf.js
+++ karma.conf.js
@@ -1,75 +1,97 @@
 // Karma configuration
-// Generated on Tue Jul 23 2013 13:58:12 GMT-0700 (PDT)
+// Generated on Tue Dec 10 2013 17:46:22 GMT-0800 (PST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+    plugins: [
+      'karma-*'
+    ],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'public/js/angular.min.js',
+      'public/js/angular-strap.min.js',
+      'public/js/services.js',
+      'test/lib/*.js',
+      'test/unit/*.js'
+    ],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'public/js/angular.min.js',
-  'public/js/angular-strap.min.js',
-  'public/js/services.js',
-  'test/lib/*.js',
-  'test/unit/*.js'
-];
+    // list of files to exclude
+    exclude: [
+    ],
 
 
-// list of files to exclude
-exclude = [
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'junit', 'coverage'],
 
-];
+    preprocessors: {
+      'public/js/main.js': ['coverage'],
+      'public/js/services.js': ['coverage'],
+      'public/tools/*.js': ['coverage'],
+      'public/tools/**/*.js': ['coverage']
+    },
+
+    coverageReporter: {
+      reporters: [
+        {
+          type: 'cobertura',
+          dir: 'coverage/',
+          file: 'cobertura-coverage.xml'
+        }, {
+          type: 'text-summary'
+        }
+      ]
+    },
+
+    // the default configuration
+    junitReporter: {
+      outputFile: 'xunit.xml'
+    },
+
+    // web server port
+    port: 9876,
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // enable / disable colors in the output (reporters and logs)
+    colors: ! process.env.JENKINS_HOME,
 
-// This is turned on through the CLI for Jenkins
-junitReporter = {
-  outputFile: 'junit-results.xml'
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: !! process.env.JENKINS_HOME
+  });
 };
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
